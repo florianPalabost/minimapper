714c781ce9dd50a290652e09872437b6542cbe40
florian <florian.palabost@etu.univ-lyon1.fr>
2020-01-14 20:31:44 +0100

    conf

---
+++ .modernizrrc.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+    options: [
+      "setClasses"
+    ],
+    "feature-detects": [
+      "battery",
+      "canvas",
+      "event/deviceorientation-motion",
+      "geolocation",
+      "storage/localstorage",
+      "pointerevents",
+      "vibration"
+    ]
+};
